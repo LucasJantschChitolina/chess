8a08edefce946eb3a22f78cd58cb32cc2b3814ea
Lucas Chitolina <lucas.chitolina01@gmail.com>
2025-07-27 11:18:17 -0300

    Add chess board rendering and initial setup
    - Implemented a function to display the initial chess board layout using a new boardMatrix.
    - Created a Square component to represent individual squares on the board.
    - Updated the App component to render the chess board.
    - Introduced a new chess.ts file to define the boardMatrix for chess pieces.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import type { JSX } from 'react/jsx-runtime'
+import { boardMatrix } from './lib/chess'
+
+function showInitialBoard() {
+  const board: JSX.Element[] = []
+
+  for (let line = 0; line < 8; line++) {
+    boardMatrix[line].map((value, index) => {
+      board.push(<Square key={Math.random()} house={index + 1} line={line + 1} value={value} />)
+    })
+  }
+
+  return board
+}
+
+function Square({ value, line, house }: { value: string, line: number, house: number }) {
+  const isLightSquare = (line % 2 === 0 && house % 2 === 0) || (line % 2 !== 0 && house % 2 !== 0)
+
+  return <div className={`border border-black w-full h-full flex flex-col items-center justify-center ${isLightSquare ? 'bg-white text-black' : 'bg-black text-white'}`}>{value}</div>
+}
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Chess</Button>
+    <div className="flex flex-col items-center justify-center h-screen bg-black">
+      <div className='grid grid-cols-8 border border-white w-[700px] h-[700px]'>
+        {showInitialBoard()}
+      </div>
     </div>
   )
 }
---
+++ src/lib/chess.ts
@@ -0,0 +1,10 @@
+export const boardMatrix = [
+  ['b_rook_1', 'b_horse_1', 'b_bishop_1', 'b_queen', 'b_king', 'b_bishop_2', 'b_horse_2', 'b_rook_2'],
+  ['b_pawn_1', 'b_pawn_2', 'b_pawn_3', 'b_pawn_4', 'b_pawn_5', 'b_pawn_6', 'b_pawn_7', 'b_pawn_8'],
+  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  ['w_pawn_1', 'w_pawn_2', 'w_pawn_3', 'w_pawn_4', 'w_pawn_5', 'w_pawn_6', 'w_pawn_7', 'w_pawn_8'],
+  ['w_rook_1', 'w_horse_1', 'w_bishop_1', 'w_queen', 'w_king', 'w_bishop_2', 'w_horse_2', 'w_rook_2'],
+]
